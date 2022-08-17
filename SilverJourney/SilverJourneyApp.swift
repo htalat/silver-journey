db8cb7e56b078454de44c485790316e577548ad5
Hassan Talat <hassan.talat.92@gmail.com>
2022-08-17 00:15:09 -0700

    Initial Commit

---
+++ SilverJourney/SilverJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SilverJourneyApp.swift
+//  SilverJourney
+//
+//  Created by Hassan Talat on 8/17/22.
+//
+
+import SwiftUI
+
+@main
+struct SilverJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
